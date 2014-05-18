84581e45de7e4300c242f62d0d043be4c5beb7ac
root <root@wanghai-TW8-SW8-DW8.(none)>
2014-05-18 16:49:39 +0800

    add my often shell scrupt path to /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/wanghai/my_often_shell:/home/wanghai/my_often_shell/intel

---
+++ add-env.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+#######################################
+# Author: hai.wang 
+#######################################
+
+current_path=`pwd`
+
+#echo $current_path
+#ls $current_path
+
+###### 1. main func ######
+my_bin_path_loop=0
+MY_SHELL_BIN_PATH=$current_path
+
+for my_bin_dir in `ls $current_path`
+do
+	if [ -d $my_bin_dir ]
+	then
+		((my_bin_path_loop++))
+		tmp_my_bin_path=$current_path/$my_bin_dir
+		MY_SHELL_BIN_PATH=$MY_SHELL_BIN_PATH:$tmp_my_bin_path
+		#echo $MY_SHELL_BIN_PATH
+	fi
+ 
+done
+################
+#has_set_my_shell_bin_path=1
+GLOBLE_ENV_FILE="/etc/bash.bashrc"
+has_add_globle_env=0
+
+##### if set flag, the following code will not be executed #####
+
+for read_my_shell_bin_path in `cat $GLOBLE_ENV_FILE`
+do
+
+if [[ "$read_my_shell_bin_path" =~ "has_set_my_shell_bin_path=1" ]];then
+#   echo has_add_globle_env=1
+   has_add_globle_env=1
+fi
+
+done
+
+if [ $has_add_globle_env -eq 1 ]
+then
+	echo "you have set the 'often use shell bin path in $GLOBLE_ENV_FILE' file !!"
+	echo "you need do '. $GLOBLE_ENV_FILE' , let you add all commands to take effect !!"
+	exit 0
+else
+	echo "###### add by wanghai, add my often use shell scrupt ###### " >> $GLOBLE_ENV_FILE
+#	echo "set my often use shell scrupt path : has_set_my_shell_bin_path=1"
+#	echo "has_set_my_shell_bin_path=1 >>  $GLOBLE_ENV_FILE"
+	echo "has_set_my_shell_bin_path=1" >>  $GLOBLE_ENV_FILE
+	
+#	echo "set my often use shell bin path!"
+#	echo "export PATH=$PATH:$MY_SHELL_BIN_PATH >> $GLOBLE_ENV_FILE"
+	echo "export PATH=$PATH:$MY_SHELL_BIN_PATH" >> $GLOBLE_ENV_FILE
+	echo >> $GLOBLE_ENV_FILE
+	echo >> $GLOBLE_ENV_FILE
+	echo >> $GLOBLE_ENV_FILE
+	echo "set often use shell scrupt path success!!"
+	echo "you need do '. $GLOBLE_ENV_FILE' , let you add all commands to take effect !!"
+fi
+
+
+
