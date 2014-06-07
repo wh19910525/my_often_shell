bebf10b5d3b2af4d79471cf0d228846b8c27fa16
hai.wang <git-admin-wh@weibu.com>
2014-06-08 00:27:38 +0800

    add : zgj1_create_all_dir_git_for_gitserver.sh zgj2_push_android_code_to_git_server.sh zgj3_get_android_source_code_from_git_server.sh zgj4_repo.sh

---
+++ zgj3_get_android_source_code_from_git_server.sh
@@ -0,0 +1,62 @@
+
+para1=$1
+loop=1
+current_path=`pwd`
+current_source_code_top_dir=$current_path/$para1
+current_data=`date "+%Y_%m_%d_%H_%M_%S"`
+
+
+#############################
+
+android_top_dir=android_source_code_$current_data
+git_server_addr=192.168.2.5
+on_git_server_android_source_code_name=all_sub_dir_git_init_2014_06_07
+
+Step1=1
+if [ $Step1 -eq 1 ]; then
+if [ $# -eq 1 ]; then
+
+mkdir $android_top_dir -p
+cd $android_top_dir
+
+
+    echo -en "\033[32m"
+
+    date "+%Y-%m-%d %H:%M:%S"
+    echo "Start get android source code from $git_server_addr:$on_git_server_android_source_code_name "
+
+    echo -en "\033[0m"
+
+
+for tmp_git_name in `cat $current_source_code_top_dir`
+do
+
+    #echo git clone  git@$git_server_addr:$on_git_server_android_source_code_name/$tmp_git_name
+    git clone  git@$git_server_addr:$on_git_server_android_source_code_name/$tmp_git_name
+    echo "$loop : Clone all_sub_dir_git_init_2014_06_07/$tmp_git_name successed!!"
+
+    echo 
+    echo 
+
+    ((loop++))
+done
+
+if [ -e android_top ]; then
+    pwd
+    mv android_top/* .
+    mv android_top/.git* .
+    
+    rm android_top -rf
+fi
+
+
+    echo -en "\033[35m"
+
+    echo "Finshed : get android source code from $git_server_addr:$on_git_server_android_source_code_name !!"
+    date "+%Y-%m-%d %H:%M:%S"
+
+    echo -en "\033[0m"
+fi
+fi
+
+
