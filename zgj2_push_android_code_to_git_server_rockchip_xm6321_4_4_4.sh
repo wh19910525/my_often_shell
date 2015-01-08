dd42f34a8caf3f75db0ba0f07ee27d21df686c48
hai.wang <git-admin-wh@weibu.com>
2015-01-08 13:46:47 +0800

    add zhonglongcai read code

--- zgj2_push_android_code_to_git_server_rockchip_xm6321_4_4_4.sh
+++ zgj2_push_android_code_to_git_server_rockchip_xm6321_4_4_4.sh
@@ -14,7 +14,7 @@ git_repositories_name_prefix="rk_intel_444"
 current_source_code_top_dir=`which $android_source_code_list`
 write_git_source_codei_list="wb_git_gly@pbserver wanghai_157 wanghaifei legang ShuaiNanXiang ChenYinJun DingYuBin LeGang_r yuanhaiping xinkun liuxingde qichaomin zhangjuyuan zjk zhaojingrong laiyuanzhao liuwei huangsheng panyiwen hulin"
 #write_git_source_codei_list="wb_git_gly@pbserver wanghai_157 qichaomin huangsheng panyiwen liuwei liuxingde hulin"
-read_git_source_code_list="tanglei"
+read_git_source_code_list="tanglei zhonglongcai"
 
 
 Step1=1
