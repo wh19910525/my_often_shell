68d10f1b05e42365d133ad9274b24d5010595bdc
hai.wang <wanghai@weibu.com>
2015-02-05 09:16:28 +0800

    add baytrain_5.0 manifest

--- repo1_from_manifest_get_ok_git_on_git_server.sh
+++ repo1_from_manifest_get_ok_git_on_git_server.sh
@@ -3,20 +3,39 @@
 #author:wanghai
 #############################
 
+Para0=$0
+Para1=$1
+
+if [ $# -eq 0 ]; then 
+    echo "Usage: $Para0 version_name"
+    exit 2
+fi
 
 loop=1
 current_path=`pwd`
 current_data=`date "+%Y_%m_%d_%H_%M_%S"`
-from_manifest_get_ok_git_on_git_server_dir=$current_path/intel_sofia_3g_5_0
+if [ $# -eq 1 ]; then 
+    from_manifest_get_ok_git_on_git_server_dir=$current_path/$Para1
+else
+    from_manifest_get_ok_git_on_git_server_dir=$current_path/intel_sofia_3g_5_0
+fi
 manifest_file=$current_path/.repo/manifest.xml
 exist_git_repositories_top_dir=$current_path/.repo/projects
 on_git_server_project_path=
 on_git_server_project_name=
 current_branch=master_2015_02_02
-manifest_repositories_name=sofia_3g_manifest
+if [ $# -eq 1 ]; then 
+    manifest_repositories_name=${Para1}_manifest
+else
+    manifest_repositories_name=sofia_3g_manifest
+fi
 manifest_def_xml=$from_manifest_get_ok_git_on_git_server_dir/$manifest_repositories_name/default.xml
 origin_frank=origin_frank
-manifest_fetch="git@192.168.2.5:intel_sofia_3g_5_0"
+if [ $# -eq 1 ]; then 
+    manifest_fetch="git@192.168.2.5:$Para1"
+else
+    manifest_fetch="git@192.168.2.5:intel_sofia_3g_5_0"
+fi
 
 
 create_all_child_project(){
@@ -107,7 +126,7 @@ create_manifest_res(){
 }
 
 ######### main #########
-#create_all_child_project
+create_all_child_project
 create_manifest_res
 
 
--- repo2_baytrain_on_git_server_repo_list.sh
+++ repo2_baytrain_on_git_server_repo_list.sh
@@ -7,22 +7,16 @@
 loop=1
 current_path=`pwd`
 current_data=`date "+%Y_%m_%d_%H_%M_%S"`
-from_manifest_get_ok_git_on_git_server_dir=$current_path/from_manifest_get_ok_git_on_git_server_dir
 manifest_file=$current_path/.repo/manifest.xml
 exist_git_repositories_top_dir=$current_path/.repo/projects
 on_git_server_project_path=
 on_git_server_project_name=
 current_branch=master_2015_02_02
-manifest_repositories_name=sofia_3g_manifest.git
-manifest_def_xml=$from_manifest_get_ok_git_on_git_server_dir/$manifest_repositories_name/default.xml
 origin_frank=origin_frank
+project_repo_list_name=baytrain_5_0_repo_res_list
 
 
 create_all_child_project(){
-Step1=1 
-if [ $Step1 -eq 1 ]; then 
-    mkdir -p $from_manifest_get_ok_git_on_git_server_dir
-fi
 
 cat $manifest_file | while read ev_line
 do
@@ -48,9 +42,9 @@ do
                 #echo "tmp4_project_name=$tmp4_project_name"
 
                 if [ $loop -eq 1 ]; then 
-                    echo "$tmp2_project_name" > sofia_3g_5_0_git_list
+                    echo "$tmp2_project_name" > $project_repo_list_name
                 else
-                    echo "$tmp2_project_name" >> sofia_3g_5_0_git_list
+                    echo "$tmp2_project_name" >> $project_repo_list_name
                 fi
                 ((loop++))
 
@@ -72,7 +66,7 @@ done
 }
 
 create_manifest_res(){
-    echo "sofia_3g_manifest" >> sofia_3g_5_0_git_list
+    echo "baytrain_5_0_manifest" >> $project_repo_list_name
 }
 
 ######### main #########
