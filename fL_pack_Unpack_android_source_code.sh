49fda51f65657f0b9834df4d1e57b22f28f3fc89
wanghai <hai.wang@archermind.com>
2016-03-14 16:20:04 +0800

    1. replace .dir .file 2. link

--- fL_pack_Unpack_android_source_code.sh
+++ fL_pack_Unpack_android_source_code.sh
@@ -27,6 +27,9 @@ save_Unpack_aosp_file_path="$current_path/$Unpack_aosp_dir"
 
 top_dir_other_file=top_dir_other_file
 
+start_dot="."
+replace_start_doc="my_dot"
+has_letter_count=`expr length $replace_start_doc`
 
 ####### funtion1 ########
 start_color_value=31
@@ -55,8 +58,8 @@ Usage(){
 Pack_Split_File(){
     #tar cvzf - lisa_test_project/ |split -b 1024m -d - logs.tar.gz.
     #echo "tar cvzf - $1/ |split -b $2 -d - $1.tar.gz."
-    echo "Start pack $1 ..."
-    tar czf - $1/ |split -b $2 -d - $1.tar.gz.
+    echo "Start pack [$1] ..."
+    tar czf - $1/ |split -b $2 -d - $3.tar.gz.
 }
 
 ####### funtion4 ########
@@ -92,13 +95,31 @@ Pack_aosp_sourc_code (){
     
     for every_file in `ls -A` 
     do
-        if [ -d $every_file/ ];then
-            mkdir -p $save_pack_aosp_file_path/$every_file
-            Pack_Split_File $every_file $every_tarFile_size
-            mv $every_file.tar.gz* $save_pack_aosp_file_path/$every_file
+        get_first_letter=${every_file:0:1}
+        del_first_letter=${every_file:1}
+        replace_start_doc_file_name=${replace_start_doc}$every_file
+
+        targer_file_name=
+
+        if [ -d $every_file ];then
+            if [ $get_first_letter == $start_dot ];then
+                targer_file_name=$replace_start_doc_file_name
+            else
+                targer_file_name=$every_file
+            fi
+
+            Pack_Split_File $every_file $every_tarFile_size $targer_file_name
+            mkdir -p $save_pack_aosp_file_path/$targer_file_name
+            mv ${targer_file_name}.tar.gz* $save_pack_aosp_file_path/$targer_file_name
+
         else # top file
             mkdir -p $save_pack_aosp_file_path/$top_dir_other_file
-            cp $every_file $save_pack_aosp_file_path/$top_dir_other_file
+            if [ $get_first_letter == $start_dot ];then
+                echo "First letter is dot file = [$every_file]"
+                cp $every_file $save_pack_aosp_file_path/$top_dir_other_file/${replace_start_doc}${every_file}
+            else
+                cp $every_file $save_pack_aosp_file_path/$top_dir_other_file -d
+            fi
         fi
 
         cd $current_path/$aosp_dir_path
@@ -129,13 +150,27 @@ Unpack_aosp_sourc_code (){
     do
         if [ -d $every_file/ ];then
             echo "Unpacking $every_file"
-            if [ $every_file != $top_dir_other_file ];then
-                cd $every_file
-                #cat logs.tar.gz.0* | tar xvz
+            cd $every_file
+
+            if [ $every_file != $top_dir_other_file ];then #get all sub dir
                 cat $every_file.tar.gz.* | tar xz -C $save_Unpack_aosp_file_path
-            else # top file
-                cp $every_file/. -a $save_Unpack_aosp_file_path
+            else #get all top file
+                echo "Get all top file ..."
+
+                for get_tmp_file in `ls -A` 
+                do
+                    get_start_prefix=${get_tmp_file:0:$has_letter_count}
+                    del_prefilx_file_name=${get_tmp_file:$has_letter_count}
+                    if [ $get_start_prefix == $replace_start_doc ];then
+                        cp $get_tmp_file $save_Unpack_aosp_file_path/$del_prefilx_file_name
+                    else
+                        cp $get_tmp_file -a $save_Unpack_aosp_file_path
+                    fi
+
+                done
             fi
+        else
+            echo "Handle some tmp file ..."
         fi
 
         cd $current_path/$1
